dbfed30cb7fcffcb5a6ece3b02a810d855c9e4ef
singerxt <mat.spiewak@gmail.com>
2015-03-08 09:10:05 +0100

    updated readme and event simulation

--- Readme.md
+++ Readme.md
@@ -1,6 +1,24 @@
 # Reproduce.js
 
-Record your bug!
+Simple script which allow you to record your behaviours in browser.
+
+## Usage
+
+In browser console
+
+```
+record() // Start recording
+stop() // Stop recording
+play() // Play saved data
+getData() // return all collected data.
+setData(data) // set data
+
+```
+
+## Support 
+
+For now supported is only Google Chrome because in near future this script will be rewrited to Chrome Extension.
+
 
 # Development
 
--- dev/bundle.js
+++ dev/bundle.js
@@ -188,9 +188,10 @@ eventSim.click = function (x,y){
   var ev = document.createEvent('MouseEvent'),
       el = document.elementFromPoint(x - 5,y);
   
-  /*
-   *
-   */
+  if(el === undefined) {
+    return false;
+  }
+
   el.style.opacity = '0.5';
   setTimeout(function () {
     el.style.opacity = '1.0';
@@ -213,6 +214,11 @@ eventSim.click = function (x,y){
 
 eventSim.mouseover = function (x,y,type) {
   var el = document.elementFromPoint(x - 5,y);
+  
+  if(el === undefined || typeof el.className.indexOf !== 'function') {
+    return false;
+  }
+
   if(el.className.indexOf('reproduce-hover') === -1) {
     el.className += ' reproduce-hover';
   } else {
